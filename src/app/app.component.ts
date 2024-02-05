d96fa6dd69506a81068fd18868b2358437f41837
Родных Сергей Георгиевич <rsg@monitel.local>
2024-02-05 15:40:35 +0300

    init: start temization project

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+
+  sayHello() {
+    console.log('Hello ', this.title, '!');
+  }
+}
