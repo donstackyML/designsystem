78eb76f0f54933cc248331acee06f46594112a18
Rodnykh Sergey <sergei.rodnykh@monitel.ru>
2024-02-12 13:01:16 +0300

    feat: add editors layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DxButtonModule, DxTextBoxModule,DxColorBoxModule, DxSelectBoxModule, DxNumberBoxModule, DxLookupModule, DxTagBoxModule, DxButtonGroupModule } from 'devextreme-angular';
+import { DevExtremeModule, DxDateBoxModule } from 'devextreme-angular';
 import { EditorsComponent } from './components/editors/editors.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
-
 
 @NgModule({
   declarations: [
@@ -16,14 +15,8 @@ import { ButtonsComponent } from './components/buttons/buttons.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DxButtonModule,
-    DxTextBoxModule,
-    DxColorBoxModule,
-    DxSelectBoxModule,
-    DxNumberBoxModule,
-    DxLookupModule,
-    DxTagBoxModule,
-    DxButtonGroupModule
+    DevExtremeModule,
+    DxDateBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/editors/editors.component.ts
+++ src/app/components/editors/editors.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+const data = [ 'checked', 'unchecked' ];
+
 @Component({
   selector: 'app-editors',
   templateUrl: './editors.component.html',
   styleUrls: ['./editors.component.css']
 })
-export class EditorsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class EditorsComponent {
+  radioData = data;
+  start = '2/1/2024';
+  end = '2/29/2024';
 }
