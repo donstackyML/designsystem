b90865ebcdb96556786038ab4c793b7b7fa08adf
Rodnykh Sergey <sergei.rodnykh@monitel.ru>
2024-02-16 16:25:29 +0300

    fix: correct global styles

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+// import { DxTabsComponent } from 'devextreme-angular';
+import { Tab, TabsService } from 'src/app/service/tabs.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css']
+  styleUrls: ['./tabs.component.css'],
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent {
+  tabsWithText: Tab[];
 
-  constructor() { }
+  tabsWithIconAndText: Tab[];
 
-  ngOnInit(): void {
+  width = 'auto';
+
+  showNavButtons = false;
+
+  scrollByContent = false;
+
+  widgetWrapperClasses = {
+    'widget-wrapper': true,
+    'widget-wrapper-horizontal': true,
+    'widget-wrapper-vertical': false,
+    'strict-width': false,
+  };
+
+  constructor(service: TabsService) {
+    this.tabsWithText = service.getTabsWithText();
+    this.tabsWithIconAndText = service.getTabsWithIconAndText();
   }
-
 }
