81b5cef298aaa60a4ba319438706f57cf88757fc
Vaghab <131035233+Vaghab@users.noreply.github.com>
2024-07-16 09:50:59 +0300

    feat: добавлены свойства maxHeight и maxWidth, изменена стилизация представления

--- src/app/directives/tooltip.directive.ts
+++ src/app/directives/tooltip.directive.ts
@@ -14,19 +14,31 @@ import {
   SecurityContext, EmbeddedViewRef
 } from '@angular/core';
 import { DxTooltipComponent } from 'devextreme-angular/ui/tooltip';
-import {DomSanitizer} from "@angular/platform-browser";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Directive({
-  selector: '[meTooltip]'
+  selector: '[meTooltip]',
 })
 export class TooltipDirective implements OnInit, OnDestroy, OnChanges {
   @Input() tooltipContent: string = '';
   @Input() tooltipPosition: 'top' | 'bottom' | 'left' | 'right' = 'top';
   @Input() tooltipClass: string = '';
   @Input() tooltipWidth: number | string = 'auto';
+  @Input() tooltipMaxWidth: number | string = 'auto';
   @Input() tooltipHeight: number | string = 'auto';
-  @Input() tooltipShowAnimation: any = { type: 'fade', from: 0, to: 1, duration: 300 };
-  @Input() tooltipHideAnimation: any = { type: 'fade', from: 1, to: 0, duration: 300 };
+  @Input() tooltipMaxHeight: number | string = 'auto';
+  @Input() tooltipShowAnimation: any = {
+    type: 'fade',
+    from: 0,
+    to: 1,
+    duration: 300,
+  };
+  @Input() tooltipHideAnimation: any = {
+    type: 'fade',
+    from: 1,
+    to: 0,
+    duration: 300,
+  };
   @Input() tooltipTemplateRef!: TemplateRef<any>;
 
   private tooltipComponentRef!: ComponentRef<DxTooltipComponent>;
@@ -51,8 +63,14 @@ export class TooltipDirective implements OnInit, OnDestroy, OnChanges {
       if (changes['tooltipWidth']) {
         instance.width = this.tooltipWidth;
       }
+      if (changes['tooltipMaxWidth']) {
+        instance.maxWidth = this.tooltipMaxWidth;
+      }
       if (changes['tooltipHeight']) {
         instance.height = this.tooltipHeight;
+      }
+      if (changes['tooltipMaxHeight']) {
+        instance.maxHeight = this.tooltipMaxHeight;
       }
       if (changes['tooltipShowAnimation'] || changes['tooltipHideAnimation']) {
         instance.animation = {
@@ -85,13 +103,17 @@ export class TooltipDirective implements OnInit, OnDestroy, OnChanges {
   }
 
   private initializeTooltip() {
-    this.tooltipComponentRef = this.viewContainerRef.createComponent(DxTooltipComponent);
+    this.tooltipComponentRef =
+      this.viewContainerRef.createComponent(DxTooltipComponent);
     const instance = this.tooltipComponentRef.instance;
 
     instance.target = this.element.nativeElement;
     instance.position = this.tooltipPosition;
     instance.width = this.tooltipWidth;
+    instance.maxWidth = this.tooltipMaxWidth;
     instance.height = this.tooltipHeight;
+    instance.maxHeight = this.tooltipMaxHeight;
+
     instance.animation = {
       show: this.tooltipShowAnimation,
       hide: this.tooltipHideAnimation
@@ -109,32 +131,36 @@ export class TooltipDirective implements OnInit, OnDestroy, OnChanges {
     instance.visible = false;
   }
 
-
   private updateTooltipContent() {
-  if (this.tooltipComponentRef) {
-    const instance: DxTooltipComponent = this.tooltipComponentRef.instance;
+    if (this.tooltipComponentRef) {
+      const instance: DxTooltipComponent = this.tooltipComponentRef.instance;
 
-    if (this.tooltipTemplateRef) {
-      instance.contentTemplate = (contentElement: any) => {
-        const viewRef: EmbeddedViewRef<any> = this.tooltipTemplateRef.createEmbeddedView({});
-        contentElement.appendChild(viewRef.rootNodes[0]);
-        return contentElement;
-      };
-    } else if (this.tooltipContent) {
-      instance.contentTemplate = () => {
-        const contentElement = this.renderer.createElement('div');
+      if (this.tooltipTemplateRef) {
+        instance.contentTemplate = (contentElement: any) => {
+          const viewRef: EmbeddedViewRef<any> =
+            this.tooltipTemplateRef.createEmbeddedView({});
+          contentElement.appendChild(viewRef.rootNodes[0]);
+          return contentElement;
+        };
+      } else if (this.tooltipContent) {
+        instance.contentTemplate = () => {
+          const contentElement = this.renderer.createElement('div');
 
-        let safeContent: string = this.sanitizer.sanitize(SecurityContext.HTML, this.tooltipContent) || '';
+          let safeContent: string =
+            this.sanitizer.sanitize(
+              SecurityContext.HTML,
+              this.tooltipContent
+            ) || '';
 
-        this.renderer.setProperty(contentElement, 'innerHTML', safeContent);
+          this.renderer.setProperty(contentElement, 'innerHTML', safeContent);
 
-        return contentElement;
-      };
-    } else {
-      instance.contentTemplate = null;
+          return contentElement;
+        };
+      } else {
+        instance.contentTemplate = null;
+      }
     }
   }
-}
 
   private destroyTooltip() {
     if (this.tooltipComponentRef) {
