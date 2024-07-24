767661f3f9a51ae3a4de7e8daec9eb0a8ea321c9
Vaghab <131035233+Vaghab@users.noreply.github.com>
2024-07-24 16:13:22 +0300

    feat: popover изменен вид страницы представления, удалены лишние методы и переменные

--- src/app/directives/popover.directive.ts
+++ src/app/directives/popover.directive.ts
@@ -1,41 +1,9 @@
-import { Directive, ElementRef, Input, OnInit, OnDestroy, Renderer2, TemplateRef, OnChanges, SimpleChanges } from '@angular/core';
-import { DxPopoverComponent } from 'devextreme-angular/ui/popover';
+import { Directive } from '@angular/core';
 
 @Directive({
-  selector: '[mePopover]'
+  selector: '[mePopover]',
+  host: {
+    '[class.me-popover]': 'true',
+  },
 })
-export class MePopoverDirective implements OnInit, OnChanges {
-  @Input() customClass: string | undefined;
-  @Input() maxWidth: number | string | undefined;
-  @Input() minWidth: number | string | undefined;
-
-  constructor(
-    private element: ElementRef,
-    private dxPopover: DxPopoverComponent
-  ) {}
-
-  ngOnInit() {
-    this.applyCustomizations();
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    this.applyCustomizations();
-  }
-
-  private applyCustomizations() {
-    const popoverInstance = this.dxPopover.instance;
-
-    if (this.customClass) {
-      this.element.nativeElement.classList.add(this.customClass);
-    }
-
-    if (this.maxWidth !== undefined) {
-      popoverInstance.option('maxWidth', this.maxWidth);
-    }
-
-    if (this.minWidth !== undefined) {
-      popoverInstance.option('minWidth', this.minWidth);
-    }
-  }
-
-}
+export class MePopoverDirective {}
