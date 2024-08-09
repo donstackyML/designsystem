4844024fa47e2a8c9dbec43f74194716defcc0ed
y.markov <y.markov@ispsystem.com>
2024-08-09 14:48:14 +0300

    fix: fix calendar

--- projects/dx-monitel-components/src/lib/directives/me-calendar/calendar.directive.ts
+++ projects/dx-monitel-components/src/lib/directives/me-calendar/calendar.directive.ts
@@ -1,101 +1,86 @@
 import { DxCalendarComponent } from 'devextreme-angular';
-import {
-  CalendarZoomLevel,
-  FirstDayOfWeek,
-  ValueChangedEvent,
-  WeekNumberRule,
-} from 'devextreme/ui/calendar';
 import { Subscription } from 'rxjs';
-
 import {
   Directive,
   EventEmitter,
-  HostListener,
+  Input,
   OnChanges,
   OnDestroy,
   OnInit,
   Output,
   SimpleChanges,
 } from '@angular/core';
+import { CalendarZoomLevel, FirstDayOfWeek, WeekNumberRule } from 'devextreme/ui/calendar';
 
 @Directive({
   selector: '[meCalendar]',
   host: {
     '[class.me-calendar]': 'true',
-    '[class.me-calendar-show-weeks-numbers]': 'isShowWeekNumbers',
+    '[class.me-calendar-show-weeks-numbers]': 'showWeekNumbers',
   },
 })
 export class MeCalendarDirective implements OnInit, OnChanges, OnDestroy {
-  @Output() onDateValueChanged = new EventEmitter<ValueChangedEvent>();
+  @Input() showWeekNumbers: boolean = true;
+  @Input() firstDayOfWeek: FirstDayOfWeek = 1;
+
+  @Output() onDateValueChanged = new EventEmitter<any>();
   @Output() showWeekNumbersChange = new EventEmitter<boolean>();
-  @Output() disabledChange = new EventEmitter<boolean>();
   @Output() firstDayOfWeekChange = new EventEmitter<FirstDayOfWeek>();
   @Output() zoomLevelChange = new EventEmitter<CalendarZoomLevel>();
   @Output() weekNumberRuleChange = new EventEmitter<WeekNumberRule>();
 
   private subscriptions: Subscription[] = [];
 
-  private isShowWeekNumbers: boolean = true;
-
   constructor(private dxCalendarComponent: DxCalendarComponent) {}
 
   ngOnInit() {
-    this.updateCalendarProperties();
+    this.updateCalendarOptions();
     this.setupEventListeners();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.updateCalendarProperties();
+    this.updateCalendarOptions();
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
-  private updateCalendarProperties() {
-    if (this.dxCalendarComponent && this.dxCalendarComponent.instance) {
-      const instance = this.dxCalendarComponent.instance;
-      instance.option('showWeekNumbers', true);
-      instance.option('firstDayOfWeek', 1);
+  private updateCalendarOptions() {
+    const instance = this.dxCalendarComponent?.instance;
+    if (instance) {
+      instance.option('showWeekNumbers', this.showWeekNumbers);
+      instance.option('firstDayOfWeek', this.firstDayOfWeek);  // Используем FirstDayOfWeek
     }
   }
 
   private setupEventListeners() {
-    if (this.dxCalendarComponent) {
+    const instance = this.dxCalendarComponent?.instance;
+    if (instance) {
       this.subscriptions.push(
         this.dxCalendarComponent.onValueChanged.subscribe((e) => {
-          this.onDateValueChanged.emit(e);
-        })
-      );
-
-      this.subscriptions.push(
-        this.dxCalendarComponent.onOptionChanged.subscribe((e) => {
-          switch (e.name) {
-            case 'showWeekNumbers':
-              this.showWeekNumbersChange.emit(e.value);
-              break;
-            case 'disabled':
-              this.disabledChange.emit(e.value);
-              break;
-            case 'firstDayOfWeek':
-              this.firstDayOfWeekChange.emit(e.value);
-              break;
-            case 'zoomLevel':
-              this.zoomLevelChange.emit(e.value);
-              break;
-            case 'weekNumberRule':
-              this.weekNumberRuleChange.emit(e.value);
-              break;
-          }
+          this.handleOptionChanged(e);
         })
       );
     }
   }
 
-  @HostListener('onOptionChanged', ['$event'])
-  onOptionChanged(e: any) {
-    if (e.name === 'showWeekNumbers') {
-      this.isShowWeekNumbers = e.value;
+  private handleOptionChanged(e: any) {
+    switch (e.name) {
+      case 'showWeekNumbers':
+        this.showWeekNumbers = e.value;
+        this.showWeekNumbersChange.emit(e.value);
+        break;
+      case 'firstDayOfWeek':
+        this.firstDayOfWeek = e.value;
+        this.firstDayOfWeekChange.emit(e.value);
+        break;
+      case 'zoomLevel':
+        this.zoomLevelChange.emit(e.value);
+        break;
+      case 'weekNumberRule':
+        this.weekNumberRuleChange.emit(e.value);
+        break;
     }
   }
 }
