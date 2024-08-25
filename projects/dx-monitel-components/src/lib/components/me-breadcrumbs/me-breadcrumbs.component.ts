66c7ae087b095c4da6b4d51a7673591b1682bceb
y.markov <y.markov@ispsystem.com>
2024-08-25 12:47:52 +0300

    feat: add breadcrumbs component

--- projects/dx-monitel-components/src/lib/components/me-breadcrumbs/me-breadcrumbs.component.ts
+++ projects/dx-monitel-components/src/lib/components/me-breadcrumbs/me-breadcrumbs.component.ts
@@ -15,25 +15,6 @@ interface BreadcrumbItem {
   imports: [CommonModule, DxDropDownButtonModule, DxButtonModule, MeIconComponent],
   template: `
     <nav class="breadcrumbs" aria-label="Breadcrumbs">
-      <dx-drop-down-button #dropDownButton
-                           *ngIf="hiddenItems.length > 0"
-                           [items]="hiddenItems"
-                           (onItemClick)="onHiddenItemClick($event)"
-                           [dropDownOptions]="{ width: 'auto' }"
-                           stylingMode="text"
-                           [width]="36"
-                           [height]="36"
-                           icon="more"
-                           displayExpr="label"
-                           keyExpr="url"
-                           [useSelectMode]="false"
-                           class="breadcrumb-dropdown"
-      >
-        <div *dxTemplate="let item of 'item'">
-          <me-icon *ngIf="item.icon" [icon]="item.icon"></me-icon>
-          <span class="label">{{ item.label }}</span>
-        </div>
-      </dx-drop-down-button>
       <ng-container *ngFor="let item of visibleItems; let last = last; let i = index">
         <dx-button
           *ngIf="!last"
@@ -51,7 +32,25 @@ interface BreadcrumbItem {
         ></dx-button>
         <span *ngIf="!last" class="separator">|</span>
       </ng-container>
+      <dx-drop-down-button #dropDownButton
+                           *ngIf="hiddenItems.length > 0"
+                           [items]="hiddenItems"
+                           (onItemClick)="onHiddenItemClick($event)"
+                           [dropDownOptions]="{ width: 'auto', minWidth: 250 }"
+                           stylingMode="text"
+                           icon="more_horiz"
+                           displayExpr="label"
+                           keyExpr="url"
+                           [useSelectMode]="false"
+                           class="breadcrumb-dropdown"
+      >
+        <div *dxTemplate="let item of 'item'">
+          <me-icon *ngIf="item.icon" [icon]="item.icon" size="small"></me-icon>
+          <span class="label">{{ item.label }}</span>
+        </div>
+      </dx-drop-down-button>
     </nav>
+
   `,
   styles: [`
     .breadcrumbs {
@@ -59,31 +58,67 @@ interface BreadcrumbItem {
       align-items: center;
       flex-wrap: wrap;
     }
+
     .separator {
       margin: 0 8px;
       color: #6c757d;
     }
+
     ::ng-deep .dx-button-mode-text {
       padding: 0;
     }
+
     ::ng-deep .dx-button-mode-text .dx-icon {
       margin-right: 4px;
     }
-    ::ng-deep .dx-button-mode-text.dx-button-has-icon .dx-icon {
-      margin-right: 4px;
-    }
+
     ::ng-deep .breadcrumb-dropdown .dx-dropdownbutton-action {
       padding: 0;
       min-width: auto;
       border: none;
       background: none;
     }
+
     ::ng-deep .breadcrumb-dropdown .dx-dropdownbutton-toggle {
       display: none;
     }
+
     ::ng-deep .breadcrumb-dropdown .dx-button-content {
       padding: 0;
     }
+
+    ::ng-deep .breadcrumb-dropdown .dx-dropdownbutton {
+      width: 36px;
+      height: 36px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    ::ng-deep .breadcrumb-dropdown .dx-button-mode-text.dx-dropdownbutton {
+      padding: 8px;
+    }
+
+    ::ng-deep .breadcrumb-dropdown .dx-icon-more_horiz {
+      font-size: 20px;
+    }
+
+    ::ng-deep .breadcrumb-dropdown .dx-dropdownbutton-popup-wrapper .dx-item-content {
+      display: flex;
+      align-items: center;
+      padding: 8px;
+    }
+
+    ::ng-deep .breadcrumb-dropdown .dx-dropdownbutton-popup-wrapper {
+      min-width: 150px;
+      right: 0; /* Позиционирование выпадающего списка справа */
+    }
+
+    ::ng-deep .breadcrumb-dropdown .dx-overlay-content.dx-dropdownbutton-popup-content {
+      transform-origin: right top; /* Выпадающий список выходит справа */
+    }
+
+
   `]
 })
 export class MeBreadcrumbsComponent {
