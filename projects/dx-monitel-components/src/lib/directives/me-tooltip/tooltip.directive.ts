7d35962dd6425e9572dc44d2047cdbf13d18eaf3
y.markov <y.markov@ispsystem.com>
2024-11-06 19:42:54 +0300

    fix: fix

--- projects/dx-monitel-components/src/lib/directives/me-tooltip/tooltip.directive.ts
+++ projects/dx-monitel-components/src/lib/directives/me-tooltip/tooltip.directive.ts
@@ -24,7 +24,7 @@ import { Position } from 'devextreme/common';
 export class MeTooltipDirective implements OnInit, OnDestroy, OnChanges {
   @Input() meTooltip: string = '';
   @Input() tooltipPosition: 'top' | 'bottom' | 'left' | 'right' = 'top';
-  @Input() tooltipClass: string = '';
+  @Input() tooltipClass: string = 'me-tooltip';
   @Input() tooltipWidth: number | string = 'auto';
   @Input() tooltipMaxWidth: number | string = 'auto';
   @Input() tooltipHeight: number | string = 'auto';
@@ -44,6 +44,7 @@ export class MeTooltipDirective implements OnInit, OnDestroy, OnChanges {
   @Input() tooltipTemplateRef!: TemplateRef<any>;
 
   private tooltipComponentRef!: ComponentRef<DxTooltipComponent>;
+  private readonly ME_TOOLTIP_CLASS = 'me-tooltip';
 
   constructor(
     private element: ElementRef,
@@ -100,7 +101,7 @@ export class MeTooltipDirective implements OnInit, OnDestroy, OnChanges {
   @HostListener('mouseleave')
   hideTooltip() {
     if (this.tooltipComponentRef && this.tooltipComponentRef.instance) {
-      this.tooltipComponentRef.instance.visible = false;
+      this.tooltipComponentRef.instance.visible = true;
     }
   }
 
@@ -124,6 +125,9 @@ export class MeTooltipDirective implements OnInit, OnDestroy, OnChanges {
     this.updateTooltipContent();
 
     const tooltipElement = this.tooltipComponentRef.location.nativeElement;
+
+    this.renderer.addClass(tooltipElement, this.ME_TOOLTIP_CLASS);
+
     if (this.tooltipClass) {
       this.renderer.addClass(tooltipElement, this.tooltipClass);
     }
