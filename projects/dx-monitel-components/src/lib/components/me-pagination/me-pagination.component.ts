52853ad00cf3eeb5be84d6339d75a0687ee87518
y.markov <y.markov@ispsystem.com>
2024-12-06 10:45:20 +0300

    fix: fix

--- projects/dx-monitel-components/src/lib/components/me-pagination/me-pagination.component.ts
+++ projects/dx-monitel-components/src/lib/components/me-pagination/me-pagination.component.ts
@@ -10,7 +10,6 @@ import {
 } from '@angular/core';
 import { DxButtonModule, DxDropDownButtonModule } from 'devextreme-angular';
 import { ItemClickEvent } from 'devextreme/ui/drop_down_button';
-import {MeButtonModule} from "dx-monitel-components";
 import { MeDropDownButtonModule } from "../../directives/me-drop-down-button/drop-down-button.module";
 import {MeSize} from "../../types/types";
 
@@ -23,7 +22,6 @@ import {MeSize} from "../../types/types";
     NgIf,
     NgClass,
     DxDropDownButtonModule,
-    MeButtonModule,
     MeDropDownButtonModule
   ],
   standalone: true,
