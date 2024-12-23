cb1d2772f9eda1df44fdd0444c8f579c16e7395a
Vaghab <131035233+Vaghab@users.noreply.github.com>
2024-12-23 17:13:56 +0300

    fix: фиксы по комментариям МЭ
    Нижний бордер у dataGrid,
    Поведение лейбла при  readOnly,
    ЧекБоксы у treeView

--- projects/dx-monitel-components/src/stories/me-tree-view/me-tree-view.stories.ts
+++ projects/dx-monitel-components/src/stories/me-tree-view/me-tree-view.stories.ts
@@ -155,6 +155,10 @@ export default {
         defaultValue: { summary: 'normal' },
       },
     },
+    selectionMode: {
+      control: 'select',
+      options: ['none', 'single', 'multiple', 'all'],
+    },
     focusStateEnabled: {
       control: 'boolean',
       description: 'Определяет состояние `focused` `TreeView` и его элементов.',
@@ -178,6 +182,7 @@ export default {
     activeStateEnabled: true,
     disabled: false,
     showCheckBoxesMode: 'normal',
+    selectionMode: 'none',
     focusStateEnabled: false,
     hoverStateEnabled: true,
     height: '300px',
