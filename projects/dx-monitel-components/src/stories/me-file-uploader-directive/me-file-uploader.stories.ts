96773359e65de798543a3c06dfbb561cdf6782a9
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-01-14 18:44:44 +0300

    fix(kp2-146): update fileUploader directive related styles

--- projects/dx-monitel-components/src/stories/me-file-uploader-directive/me-file-uploader.stories.ts
+++ projects/dx-monitel-components/src/stories/me-file-uploader-directive/me-file-uploader.stories.ts
@@ -309,16 +309,15 @@ export const AdvancedCustomDropZone: Story = {
           >
             <img id="dropzone-image" [src]="componentState.imageSource" *ngIf="componentState.imageSource" alt="" />
             <div id="dropzone-text" class="flex-box" *ngIf="componentState.textVisible">
-              <span class="me-text-caption">Drag & Drop the desired file</span>
-              <span class="me-text-caption">…or click to browse for a file instead.</span>
+              <span class="me-text-caption">Drag&Drop the desired file or click to browse for a file instead</span>
             </div>
             <dx-progress-bar
               #uploadProgress
               meProgressBar
               [min]="0"
               [max]="100"
-              width="30%"
-              [showStatus]="false"
+              width="100%"
+              [showStatus]="true"
               [visible]="componentState.progressVisible"
               [value]="componentState.progressValue"
             ></dx-progress-bar>
