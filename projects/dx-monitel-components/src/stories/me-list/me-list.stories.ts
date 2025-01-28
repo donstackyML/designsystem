7d2549fa57218f0de61d7d6bef2bdaf4af3d6ada
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-01-28 11:21:56 +0300

    fix(KP2-168): update list styles

--- projects/dx-monitel-components/src/stories/me-list/me-list.stories.ts
+++ projects/dx-monitel-components/src/stories/me-list/me-list.stories.ts
@@ -197,7 +197,7 @@ export default {
       options: [true, false],
     },
     grouped: {
-      discription: 'Группировать элементы',
+      description: 'Группировать элементы',
       control: 'select',
       options: [true, false],
     },
