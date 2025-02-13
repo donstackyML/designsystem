3bfef6ca2a452c0b7a47d8bb8b5cd3601d48b70a
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-02-13 17:39:01 +0300

    feat(kp2-214): extend me-toast API and update storybook documentation for toast component and directive

--- projects/dx-monitel-components/src/lib/components/me-toast/me-toast.component.ts
+++ projects/dx-monitel-components/src/lib/components/me-toast/me-toast.component.ts
@@ -3,6 +3,10 @@ import { DxButtonModule, DxToastComponent, DxToastModule } from 'devextreme-angu
 import { MeButtonModule } from '../../directives/me-button/me-button.module';
 import { MeToastModule } from '../../directives/me-toast/me-toast.module';
 
+import { CommonModule } from '@angular/common';
+import { AnimationConfig } from 'devextreme/animation/fx';
+import { PositionConfig } from 'devextreme/animation/position';
+import { ToastType } from 'devextreme/ui/toast';
 import { MeButtonStyle, MeButtonType, MeSize } from '../../types/types';
 import { MeIconComponent } from '../me-icon/me-icon.component';
 
@@ -13,26 +17,65 @@ const ANIMATION_DURATION_MS = 400;
   templateUrl: './me-toast.component.html',
   exportAs: "meToastControl",
   standalone: true,
-  imports: [DxToastModule, MeButtonModule, DxButtonModule, MeIconComponent, MeToastModule],
+  imports: [
+    CommonModule,
+    DxToastModule,
+    MeButtonModule,
+    DxButtonModule,
+    MeIconComponent,
+    MeToastModule
+  ],
 })
 export class MeToastComponent {
-  @Input() message: string = ''
-  @Input() title: string = ''
-  @Input() type: 'info' | 'success' | 'error' | 'warning' | 'info-inverted' = 'success';
-  @Input() size: 'small' | 'large' = 'small';
-  @Input() position: string = 'bottom right';
-  @Input() displayTime: number = 4000;
-  @Input() animation: { hide: any; show: any } = {
-    show: { type: 'fade', duration: ANIMATION_DURATION_MS, from: 0, to: 1 },
-    hide: { type: 'fade', duration: ANIMATION_DURATION_MS, from: 1, to: 0 },
-  };
-  @Input() visible: boolean = false
-
-  @Output() onAccept = new EventEmitter<string>();
-  @Output() onCancel = new EventEmitter<void>();
-  @Output() onClose = new EventEmitter<void>();
-
   @ViewChild('dxToast') dxToast?: DxToastComponent;
+
+  @Input() message: string = '';
+  @Input() title: string = '';
+
+  @Input() type: ToastType | 'info-inverted' = 'info';
+  @Input() visible: boolean = false;
+  @Input() displayTime: number = 4000;
+
+  @Input() size: 'small' | 'large' = 'small';
+  @Input() height: number | Function | string = '';
+  @Input() maxHeight: number | Function | string = '';
+  @Input() minHeight: number | Function | string = '';
+  @Input() width: number | Function | string = '260px';
+  @Input() maxWidth: number | Function | string = '';
+  @Input() minWidth: number | Function | string = '';
+
+  @Input() showIcon: boolean = true;
+  @Input() showCloseButton: boolean = true;
+  @Input() showActionButtons: boolean = false;
+
+  @Input() position: PositionConfig | string = 'bottom right';
+  @Input() animation: {
+    hide?: AnimationConfig;
+    show?: AnimationConfig;
+  } = {
+      show: { type: 'fade', duration: ANIMATION_DURATION_MS, from: 0, to: 1 },
+      hide: { type: 'fade', duration: ANIMATION_DURATION_MS, from: 1, to: 0 },
+    };
+
+  @Input() closeOnClick: boolean = false;
+  @Input() hideOnOutsideClick: boolean | ((event: Event) => boolean) = false;
+  @Input() closeOnSwipe: boolean = false;
+  @Input() hoverStateEnabled: boolean = true;
+
+  @Input() template: any;
+
+  @Output() onContentReady = new EventEmitter<any>();
+  @Output() onDisposing = new EventEmitter<any>();
+  @Output() onHidden = new EventEmitter<any>();
+  @Output() onHiding = new EventEmitter<any>();
+  @Output() onInitialized = new EventEmitter<any>();
+  @Output() onOptionChanged = new EventEmitter<any>();
+  @Output() onShowing = new EventEmitter<any>();
+  @Output() onShown = new EventEmitter<any>();
+
+  @Output() onAccept = new EventEmitter<any>();
+  @Output() onCancel = new EventEmitter<any>();
+  @Output() onClose = new EventEmitter<any>();
 
   handleAccept() {
     this.onAccept.emit();
@@ -43,16 +86,6 @@ export class MeToastComponent {
   handleClose() {
     this.onClose.emit();
   }
-
-  @Input() positions = [
-    'top left',
-    'top center',
-    'top right',
-    'bottom left',
-    'bottom center',
-    'bottom right',
-    'center',
-  ];
 
   statusIcon(): string {
     switch (this.type) {
--- src/app/components/me-toast-demo/me-toast-demo.component.ts
+++ src/app/components/me-toast-demo/me-toast-demo.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AnimationConfig } from 'devextreme/animation/fx';
+import notify from 'devextreme/ui/notify';
+import { ToastType } from 'devextreme/ui/toast';
 
 @Component({
   selector: 'me-toast-demo',
-  templateUrl: './me-toast.component.html',
-  styleUrls: ['./me-toast.component.css'],
+  templateUrl: './me-toast-demo.component.html',
+  styleUrls: ['./me-toast-demo.component.css'],
 })
-export class MeToastComponent {
+export class MeToastDemoComponent {
   message: string = 'This is a toast notification!';
   displayTime: number = 3000;
   position: any = 'bottom right';
+  toastType: ToastType = 'info';
   animation: { hide: AnimationConfig; show: AnimationConfig } = {
     show: { type: 'fade', duration: 400, from: 0, to: 1 },
     hide: { type: 'fade', duration: 400, from: 1, to: 0 },
@@ -26,6 +29,13 @@ export class MeToastComponent {
     'bottom right',
     'center',
   ];
+  toastTypes = [
+    'info',
+    'info-inverted',
+    'success',
+    'warning',
+    'error',
+  ];
 
   icons = [
     { text: 'Info', icon: 'dx-icon-info' },
@@ -33,7 +43,20 @@ export class MeToastComponent {
     { text: 'Warning', icon: 'dx-icon-warning' },
   ];
 
+  showNotification() {
+    notify({
+      displayTime: this.displayTime,
+      message: this.message,
+      type: this.toastType,
+      position: this.position,
+      width: '260px',
+    })
+  }
   showToast(toastDirective: any) {
+    toastDirective.showToast();
+  }
+
+  showToastComponent(toastDirective: any) {
     toastDirective.showToast();
   }
 
