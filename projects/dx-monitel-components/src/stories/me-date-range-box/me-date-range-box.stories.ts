adb17bb5947b958b4ba1ddcad97823c49603b170
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-02-17 19:44:07 +0300

    docs(KP2-226): update storybook documentation for DateRangeBox component

--- projects/dx-monitel-components/src/stories/me-date-range-box/me-date-range-box.stories.ts
+++ projects/dx-monitel-components/src/stories/me-date-range-box/me-date-range-box.stories.ts
@@ -8,7 +8,7 @@ import { DxDateRangeBoxModule, DxValidatorModule } from 'devextreme-angular';
 import { MeDateRangeBoxDirective, MeLabelDirective } from '../../public-api';
 
 export default {
-  title: 'Components/DateRangeBox(RC)',
+  title: 'Components/DateRangeBox',
   decorators: [
     moduleMetadata({
       declarations: [MeDateRangeBoxDirective, MeLabelDirective],
@@ -16,135 +16,274 @@ export default {
     }),
   ],
   argTypes: {
-    size: {
-      control: 'select',
-      options: ['small', 'medium', 'large'],
-    },
-    acceptCustomValue: {
-      control: 'boolean',
-      description: 'Позволяет пользователю ввести дату вручную.',
-    },
-    applyButtonText: {
+    startDateLabel: {
       control: 'text',
-      description: 'Надпись на кнопке "Применить".',
-    },
-    applyValueMode: {
-      control: 'select',
-      options: ['instantly', 'useButtons'],
-      description:
-        'Определяет способ, которым конечный пользователь применяет выбранное значение.',
-    },
-    cancelButtonText: {
-      control: 'text',
-      description: 'Надпись на кнопке "Отменить".',
-    },
-    dateSerializationFormat: {
-      control: 'text',
-      description:
-        'Формат сериализации даты. `yyyy-MM-dd` - Дата. `yyyy-MM-ddTHH:mm:ss` Локальная дата и время. `yyyy-MM-ddTHH:mm:ssZ` Дата и время в UTC. `yyyy-MM-ddTHH:mm:ssx`, `yyyy-MM-ddTHH:mm:ssxx`, `yyyy-MM-ddTHH:mm:ssxxx` Дата и время с таймзоной.',
-    },
-    disableOutOfRangeSelection: {
-      control: 'boolean',
-      description:
-        'Указывает, отключает ли компонент пользовательского интерфейса выбор даты до даты начала и после даты окончания.',
-    },
-    disabled: {
-      control: 'boolean',
-      description: 'Отключает `DateBox` и его элементы.',
-    },
-    displayFormat: {
-      control: 'text',
-      description: 'Формат, используемый для отображения информации.',
-    },
-    endDate: {
-      control: 'text',
-      description: 'Дата окончания.',
+      description: 'Метка поля ввода даты начала.',
       table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Start Date' },
+      },
+    },
+    startDatePlaceholder: {
+      control: 'text',
+      description: 'Метка поля ввода даты начала.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+      },
+    },
+    startDate: {
+      control: 'text',
+      description: 'Дата начала.',
+      table: {
+        category: 'Контент и управление контентом',
         type: { summary: 'number | Date | string' },
         defaultValue: { summary: 'null' },
       },
     },
     endDateLabel: {
       control: 'text',
-      description: 'Задает метку поля ввода даты окончания.',
+      description: 'Метка поля ввода даты окончания.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'End Date' },
+      },
     },
     endDatePlaceholder: {
       control: 'text',
-      description: 'Задает метку поля ввода даты окончания.',
+      description: 'Метка поля ввода даты окончания.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+      },
     },
-    invalidEndDateMessage: {
+    endDate: {
       control: 'text',
-      description: 'Сообщение при неверной дате окончания.',
+      description: 'Дата окончания.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'number | Date | string' },
+        defaultValue: { summary: 'null' },
+      },
     },
-    invalidStartDateMessage: {
+    applyButtonText: {
       control: 'text',
-      description: 'Сообщение при неверной дате начала.',
+      description: 'Текст на кнопке "Применить".',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'OK' },
+      },
+    },
+    cancelButtonText: {
+      control: 'text',
+      description: 'Текст на кнопке "Отменить".',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Cancel' },
+      },
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large'],
+      description: 'Определяет размер текстового поля.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'medium' },
+      },
+    },
+    width: {
+      control: 'text',
+      description: 'Ширина компонента.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'string | number | undefined' },
+        defaultValue: { summary: 'undefined' },
+      },
+    },
+    height: {
+      control: 'text',
+      description: 'Высота компонента.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'string | number | undefined' },
+        defaultValue: { summary: 'undefined' },
+      },
     },
     labelMode: {
       control: 'select',
-      options: ['static', 'floating', 'hidden', 'outside'],
-      description: 'Режим отображения метки',
+      options: ['outside', 'static', 'floating', 'hidden'],
+      description: 'Указывает, где будет размещаться лейбл.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'outside' },
+      },
+    },
+    showClearButton: {
+      control: 'boolean',
+      description: 'Показывает кнопку для очистки поля.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    multiView: {
+      control: 'boolean',
+      description: 'Показывает дату начала и дату окончания в одном поле.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    acceptCustomValue: {
+      control: 'boolean',
+      description: 'Разрешает ввод пользовательских дат.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' },
+      },
+    },
+    applyValueMode: {
+      control: 'select',
+      options: ['instantly', 'useButtons'],
+      description:
+        'Определяет способ, которым конечный пользователь применяет выбранное значение.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'instantly' },
+      },
+    },
+    dateSerializationFormat: {
+      control: 'text',
+      description: 'Формат для сериализации дат.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'string | undefined' },
+        defaultValue: { summary: 'undefined' },
+      },
+    },
+    disableOutOfRangeSelection: {
+      control: 'boolean',
+      description:
+        'Указывает, отключает ли компонент пользовательского интерфейса выбор даты до даты начала и после даты окончания.',
+      table: {
+        category: 'Контент и управление контентом',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    readOnly: {
+      control: 'boolean',
+      description: 'Определяет состояние только для чтения',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    isValid: {
+      control: 'boolean',
+      description: 'Проверяет валидность данных.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' },
+      },
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Отключает компонент и его элементы.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    displayFormat: {
+      control: 'text',
+      description: 'Формат для отображения информации.',
+      table: {
+        category: 'Внешний вид и размеры',
+        type: { summary: 'string | null' },
+        defaultValue: { summary: 'null' },
+      },
     },
     max: {
       control: 'text',
       description: 'Максимальная дата окончания.',
       table: {
-        type: { summary: 'number | Date | string' },
-        defaultValue: { summary: 'null' },
+        category: 'Валидация',
+        type: { summary: 'number | Date | string | undefined' },
+        defaultValue: { summary: 'undefined' },
       },
     },
     min: {
       control: 'text',
       description: 'Минимальная дата начала.',
       table: {
-        type: { summary: 'number | Date | string' },
-        defaultValue: { summary: 'null' },
+        category: 'Валидация',
+        type: { summary: 'number | Date | string | undefined' },
+        defaultValue: { summary: 'undefined' },
       },
-    },
-    multiView: {
-      control: 'boolean',
-      description: 'Показывает дату начала и дату окончания в одном поле.',
-    },
-    readOnly: {
-      control: 'boolean',
-      description: 'Определяет состояние только для чтения.',
-    },
-    showClearButton: {
-      control: 'boolean',
-      description: 'Показ кнопки очистки.',
-      defaultValue: false,
-    },
-    startDate: {
-      control: 'text',
-      description: 'Дата начала.',
-      table: {
-        type: { summary: 'number | Date | string' },
-        defaultValue: { summary: 'null' },
-      },
-    },
-    startDateLabel: {
-      control: 'text',
-      description: 'Задает метку поля ввода даты начала.',
     },
     startDateOutOfRangeMessage: {
       control: 'text',
       description:
         'Указывает сообщение, отображаемое, если указанная дата начала позже максимального значения или раньше минимального значения.',
-      defaultValue: 'Start date is out of range',
+      table: {
+        category: 'Валидация',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Start date is out of range' },
+      },
     },
-    startDatePlaceholder: {
+    invalidEndDateMessage: {
       control: 'text',
-      description: 'Задает метку поля ввода даты начала.',
+      description: 'Сообщение при неверной дате окончания.',
+      table: {
+        category: 'Валидация',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'End value must be a date' },
+      },
+    },
+    invalidStartDateMessage: {
+      control: 'text',
+      description: 'Сообщение при неверной дате начала.',
+      table: {
+        category: 'Валидация',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Start value must be a date' },
+      },
     },
     validationMessageMode: {
       control: 'select',
       options: ['auto', 'always'],
       description: 'Режим отображения сообщения об ошибке.',
+      table: {
+        category: 'Валидация',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'auto' },
+      },
     },
     validationMessagePosition: {
       control: 'select',
-      options: ['top', 'bottom', 'left', 'right'],
+      options: ['auto', 'top', 'bottom', 'left', 'right'],
       description: 'Позиция отображения сообщения об ошибке.',
+      table: {
+        category: 'Валидация',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'auto' },
+      },
     },
   },
   args: {
@@ -153,12 +292,12 @@ export default {
     disabled: false,
     readOnly: false,
     showClearButton: false,
-    acceptCustomValue: false,
+    acceptCustomValue: true,
     applyValueMode: 'instantly',
     applyButtonText: 'Применить',
     cancelButtonText: 'Отмена',
     validationMessageMode: 'auto',
-    validationMessagePosition: 'bottom',
+    validationMessagePosition: 'auto',
     dateSerializationFormat: 'yyyy-MM-dd',
     displayFormat: 'dd.MM.yyyy',
     startDate: null,
@@ -181,20 +320,93 @@ export default {
 				${argsToTemplate(args)}
 			>
 				<dx-validator>
-        <dxi-validation-rule
-            type="required"
-            message="Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required "
-        >
-        </dxi-validation-rule>
-    </dx-validator>
+          <dxi-validation-rule
+              type="required"
+              message="Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required Required "
+          >
+          </dxi-validation-rule>
+        </dx-validator>
 			</dx-date-range-box>
 		`,
   }),
-} as Meta;
+} satisfies Meta<DxDateRangeBoxModule | MeDateRangeBoxDirective>;
 
-export const Default: StoryObj = {};
+type Story = StoryObj<DxDateRangeBoxModule | MeDateRangeBoxDirective>
 
-export const WithLabelRow: StoryObj = {
+export const Default: Story = {};
+
+export const SizeSmall: Story = {
+  args: {
+    size: 'small',
+  },
+};
+
+export const SizeMedium: Story = {
+  args: {
+    size: 'medium'
+  }
+}
+
+export const SizeLarge: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const LabelModeFloating: Story = {
+  args: {
+    labelMode: 'floating',
+  },
+};
+
+export const LabelModeOutside: Story = {
+  args: {
+    labelMode: 'outside',
+  },
+};
+
+export const LabelModeStatic: Story = {
+  args: {
+    labelMode: 'static',
+  },
+};
+
+export const LabelModeHidden: Story = {
+  args: {
+    labelMode: 'hidden',
+  },
+};
+
+export const StateDisabled: Story = {
+  args: {
+    disabled: true
+  },
+};
+
+export const StateReadOnly: Story = {
+  args: {
+    readOnly: true,
+    startDate: '1/1/2025',
+    endDate: '1/31/2025'
+  },
+};
+
+export const StateDisabledAndReadOnly: Story = {
+  args: {
+    readOnly: true,
+    disabled: true,
+    startDate: '1/1/2025',
+    endDate: '1/31/2025'
+  },
+};
+
+export const WithMultiView: Story = {
+  args: {
+    multiView: true
+  },
+};
+
+export const WithLabelRow: Story = {
   args: {
     ...Default.args,
     labelMode: 'hidden',
