385bbdf96a70384a2988ce0d59d94997b4032255
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-02-19 16:49:11 +0300

    docs(KP2-147): update storybook documentation for LoadIndicator component

--- projects/dx-monitel-components/src/stories/me-load-indicator/me-load-indicator.stories.ts
+++ projects/dx-monitel-components/src/stories/me-load-indicator/me-load-indicator.stories.ts
@@ -1,8 +1,8 @@
-import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { argsToTemplate, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { DxLoadIndicatorModule } from 'devextreme-angular';
 import { MeLoadIndicatorDirective } from '../../public-api';
 
-const meta: Meta<MeLoadIndicatorDirective> = {
+export default {
   title: 'Components/LoadIndicator',
   component: MeLoadIndicatorDirective,
   decorators: [
@@ -13,85 +13,149 @@ const meta: Meta<MeLoadIndicatorDirective> = {
   ],
   argTypes: {
     color: {
+      description: 'Цвет индикатора загрузки.',
+      control: 'select',
       options: ['normal', 'default', 'accent'],
-      control: { type: 'select' },
-      defaultValue: 'default',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'default' },
+      },
     },
     indicatorSrc: {
       control: 'text',
+      description: 'URL изображения индикатора.',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+      },
     },
     size: {
+      description: 'Размер индикатора.',
+      control: 'select',
       options: ['small', 'medium', 'large'],
-      control: { type: 'select' },
-      defaultValue: 'medium',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'medium' },
+      },
     },
     stylingMode: {
       options: ['line', 'circle'],
-      control: { type: 'select' },
-      defaultValue: 'circle',
+      control: 'select',
+      description: 'Стиль индикатора загрузки.',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'circle' },
+      },
+    },
+    height: {
+      description: 'Высота индикатора загрузки.',
+      control: 'text',
+      table: {
+        type: { summary: 'string | number | undefined' },
+        defaultValue: { summary: 'undefined' },
+      },
+    },
+    width: {
+      description: 'Ширина индикатора загрузки.',
+      control: 'text',
+      table: {
+        type: { summary: 'string | number | undefined' },
+        defaultValue: { summary: 'undefined' },
+      },
     },
   },
-};
-
-export default meta;
-
-type Story = StoryObj<MeLoadIndicatorDirective>;
-
-const Template: Story = {
   args: {
-    color: 'normal',
-    size: 'small',
+    color: 'default',
+    size: 'medium',
     stylingMode: 'circle',
+    height: undefined,
+    width: undefined,
+    indicatorSrc: ''
   },
   render: (args) => ({
     props: args,
     template: `
-		<div style="display: flex;
-    height: 50px;
-    width: 50px;
-    justify-content: center;
-    align-items: center; background-color: var(--Background-Canvas);
-		border-radius: 4px;
-		border: 1px dashed #9747ff">
+		<div class="load-indicator-example">
 		<dx-load-indicator
 			id="small-indicator"
 			meLoadIndicator
-			[size]="size"
-			[color]="color"
-			[stylingMode]="stylingMode"
+			${argsToTemplate(args)}
 		></dx-load-indicator>
 		</div>
     `,
+    styles: [
+      `
+      .load-indicator-example {
+        display: flex;
+        padding: 20px;
+        width: fit-content;
+        height: fit-content;
+        justify-content: center;
+        align-items: center; background-color: var(--Background-Canvas);
+        border-radius: 4px;
+        border: 1px dashed #9747ff
+      }
+      `
+    ]
   }),
-};
+} satisfies Meta<DxLoadIndicatorModule  | MeLoadIndicatorDirective>;
 
-export const Default: Story = {
-  ...Template,
-};
+type Story = StoryObj<DxLoadIndicatorModule | MeLoadIndicatorDirective>;
+
+export const Default: Story = {};
 
 export const WithCustomImage: Story = {
-  render: (args) => ({
-    props: args,
-    template: `
-      <div class="label me-title-header2">Custom image</div>
-			<div style="display: flex;
-			height: 50px;
-			width: 50px;
-			justify-content: center;
-			align-items: center; background-color: var(--Background-Canvas);
-			border-radius: 4px;
-			border: 1px dashed #9747ff">
-				<dx-load-indicator
-					id="image-indicator"
-					meLoadIndicator
-					[size]="'large'"
-					[indicatorSrc]="indicatorSrc"
-				></dx-load-indicator>
-			</div>
-    `,
-  }),
   args: {
     indicatorSrc:
       'https://static.tildacdn.com/tild6261-3766-4534-a636-643635653261/6068d1f9087cdc5982a3.gif',
   },
+};
+
+export const SizeSmall: Story = {
+  args: {
+    size: 'small'
+  }
+};
+
+export const SizeMedium: Story = {
+  args: {
+    size: 'medium'
+  }
+};
+
+export const SizeLarge: Story = {
+  args: {
+    size: 'large'
+  }
+};
+
+export const StylingModeCircle: Story = {
+  args: {
+    stylingMode: 'circle'
+  }
+};
+
+export const StylingModeLine: Story = {
+  args: {
+    stylingMode: 'line'
+  }
+};
+
+
+export const ColorDefault: Story = {
+  args: {
+    color: 'default'
+  }
+};
+
+export const ColorNormal: Story = {
+  args: {
+    color: 'normal'
+  }
+};
+
+export const ColorAccent: Story = {
+  args: {
+    color: 'accent'
+  }
 };
