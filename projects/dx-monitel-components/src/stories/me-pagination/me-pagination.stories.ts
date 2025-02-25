dcd100f2d33751afd01ca0ed5a2032ec609956e8
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-02-25 11:26:37 +0300

    docs(KP2-147): update storybook documentation for Pagination component

--- projects/dx-monitel-components/src/stories/me-pagination/me-pagination.stories.ts
+++ projects/dx-monitel-components/src/stories/me-pagination/me-pagination.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { Meta, StoryObj, argsToTemplate, moduleMetadata } from '@storybook/angular';
 import { DxButtonModule, DxSelectBoxModule } from 'devextreme-angular';
 import { MePaginationComponent, MeSelectBoxModule } from '../../public-api';
 
@@ -19,86 +19,166 @@ export default {
     totalItems: {
       control: { type: 'number', min: 0, max: 1000, step: 1 },
       description: 'Общее количество элементов',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '0' },
+      },
     },
     itemsPerPage: {
       control: { type: 'number', min: 1, max: 100, step: 1 },
       description: 'Количество элементов на странице',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '10' },
+      },
     },
     currentPage: {
       control: { type: 'number', min: 1, max: 100, step: 1 },
       description: 'Текущая страница',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '1' },
+      },
+    },
+    itemsPerPageOptions: {
+      control: { type: 'object' },
+      description: 'Возможные варианты количества элементов на странице',
+      table: {
+        type: { summary: 'number[]' },
+        defaultValue: { summary: '[10, 50, 100]' },
+      },
     },
     maxVisiblePages: {
       control: { type: 'number', min: 3, max: 10, step: 1 },
       description: 'Максимальное количество видимых кнопок страниц',
-    },
-    useButtons: {
-      control: { type: 'boolean' },
-      description:
-        'Использовать кнопки вместо выпадающего списка для выбора количества элементов на странице',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '7' },
+      },
     },
     size: {
       control: 'select',
       options: ['small', 'medium', 'large'],
       description: 'Размер всех элементов управления',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'medium' },
+      },
     },
-    itemsPerPageOptions: {
-      control: { type: 'object' },
-      description: 'Возможные варианты количества элементов на странице',
+    hasPageSizeControlButtons: {
+      control: { type: 'boolean' },
+      description: 'Включено ли поле выбора количества элементов на странице',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' },
+      },
     },
-    pageChange: { action: 'pageChanged' },
-    itemsPerPageChange: { action: 'itemsPerPageChanged' },
+    useButtons: {
+      control: { type: 'boolean' },
+      description: 'Использовать кнопки вместо выпадающего списка для выбора количества элементов на странице',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
     transparentBackground: {
       control: { type: 'boolean' },
       description: 'Прозрачный фон для кнопок',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    isDarkTheme: {
+      control: { type: 'boolean' },
+      description: 'Темная тема',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    pageChange: {
+      action: 'pageChanged',
+      description: 'Событие при изменении страницы',
+      table: {
+        type: { summary: 'function' },
+      },
+    },
+    itemsPerPageChange: {
+      action: 'itemsPerPageChanged',
+      description: 'Событие при изменении количества элементов на странице',
+      table: {
+        type: { summary: 'function' },
+      },
     },
   },
   args: {
     totalItems: 100,
     itemsPerPage: 10,
     currentPage: 1,
-    maxVisiblePages: 5,
-    useButtons: false,
-    size: 'medium',
+    maxVisiblePages: 7,
     itemsPerPageOptions: [10, 50, 100],
+    size: 'medium',
+    hasPageSizeControlButtons: true,
+    useButtons: false,
   },
-} as Meta<MePaginationComponent>;
+  render: (args) => ({
+    props: args,
+    template: `
+      <me-pagination ${argsToTemplate(args)}></me-pagination>
+    `,
+  })
+} satisfies Meta<MePaginationComponent>;
 
 type Story = StoryObj<MePaginationComponent>;
 
-const Template: Story = {
-  render: (args) => ({
-    props: {
-      ...args,
-      onPageChange: (e: any) => console.log('Страница изменена', e),
-      onItemsPerPageChange: (e: any) =>
-        console.log('Изменено количество элементов на странице', e),
-    },
-    template: `
-      <me-pagination
-        [totalItems]="totalItems"
-        [itemsPerPage]="itemsPerPage"
-        [currentPage]="currentPage"
-        [maxVisiblePages]="maxVisiblePages"
-        [useButtons]="useButtons"
-        [size]="size"
-        [itemsPerPageOptions]="itemsPerPageOptions"
-        (pageChange)="onPageChange($event)"
-        (itemsPerPageChange)="onItemsPerPageChange($event)"
-        [transparentBackground]="transparentBackground"
-      >
-      </me-pagination>
-    `,
-  }),
+export const Default: Story = {};
+
+export const SizeSmall: Story = {
+  args: {
+    size: 'small',
+  },
 };
 
-export const Default: Story = {
-  ...Template,
-  args: {},
+export const SizeMedium: Story = {
+  args: {
+    size: 'medium',
+  },
+};
+
+export const SizeLarge: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const PageSizeControlWithDropDownButton: Story = {
+  args: {
+    useButtons: false,
+    itemsPerPageOptions: [10, 20, 30]
+  },
+};
+
+export const PageSizeControlWithButtons: Story = {
+  args: {
+    useButtons: true,
+    itemsPerPageOptions: [10, 20, 30]
+  },
+};
+
+export const WithoutPageSizeControlButtons: Story = {
+  args: {
+    hasPageSizeControlButtons: false
+  },
+};
+
+export const WithoutBackground: Story = {
+  args: {
+    transparentBackground: true
+  },
 };
 
 export const ManyPages: Story = {
-  ...Template,
   args: {
     totalItems: 1000,
     itemsPerPage: 20,
@@ -108,7 +188,6 @@ export const ManyPages: Story = {
 };
 
 export const FewItems: Story = {
-  ...Template,
   args: {
     totalItems: 30,
     itemsPerPage: 10,
@@ -118,18 +197,16 @@ export const FewItems: Story = {
 };
 
 export const CustomItemsPerPage: Story = {
-  ...Template,
   args: {
     totalItems: 500,
     itemsPerPage: 50,
     currentPage: 1,
     maxVisiblePages: 5,
-    useButtons: true, // Используем кнопки для выбора количества элементов на странице
+    useButtons: true,
   },
 };
 
 export const LastPage: Story = {
-  ...Template,
   args: {
     totalItems: 100,
     itemsPerPage: 10,
@@ -139,7 +216,6 @@ export const LastPage: Story = {
 };
 
 export const MaxVisiblePages: Story = {
-  ...Template,
   args: {
     totalItems: 200,
     itemsPerPage: 10,
@@ -148,26 +224,14 @@ export const MaxVisiblePages: Story = {
   },
 };
 
-export const WithLargeButtons: Story = {
-  ...Template,
-  args: {
-    totalItems: 500,
-    itemsPerPage: 50,
-    currentPage: 1,
-    maxVisiblePages: 5,
-    useButtons: true,
-    size: 'large', // Устанавливаем большие кнопки
-  },
-};
 
 export const CustomItemsOptions: Story = {
-  ...Template,
   args: {
     totalItems: 400,
     itemsPerPage: 50,
     currentPage: 2,
     maxVisiblePages: 5,
     useButtons: false,
-    itemsPerPageOptions: [25, 50, 75, 100], // Кастомные опции для выбора количества элементов на странице
+    itemsPerPageOptions: [25, 50, 75, 100],
   },
 };
