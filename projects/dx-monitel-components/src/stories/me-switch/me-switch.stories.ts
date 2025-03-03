f50a8111c8a3b37e8bfc060c21b8b08affccaeef
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-03-03 16:33:47 +0300

    docs(KP2-147): update storybook documentation for Switch component

--- projects/dx-monitel-components/src/stories/me-switch/me-switch.stories.ts
+++ projects/dx-monitel-components/src/stories/me-switch/me-switch.stories.ts
@@ -4,7 +4,7 @@ import {
   type Meta,
   type StoryObj,
 } from '@storybook/angular';
-import { DxSwitchComponent, DxValidatorModule } from 'devextreme-angular';
+import { DxSwitchComponent } from 'devextreme-angular';
 import { MeLabelDirective, MeSwitchDirective } from '../../public-api';
 
 export default {
@@ -40,28 +40,107 @@ export default {
         defaultValue: { summary: 'medium' },
       },
     },
+    switchedOffText: {
+      control: 'text',
+      description: 'Текст, отображаемый при выключенном состоянии',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '""' },
+      },
+    },
+    switchedOnText: {
+      control: 'text',
+      description: 'Текст, отображаемый при включенном состоянии',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '""' },
+      },
+    },
+    height: {
+      control: 'text',
+      description: 'Высота компонента',
+      table: {
+        type: { summary: 'string | number | undefined' },
+        defaultValue: { summary: 'undefined' },
+      },
+    },
+    width: {
+      control: 'text',
+      description: 'Ширина компонента',
+      table: {
+        type: { summary: 'string | number | undefined' },
+        defaultValue: { summary: 'undefined' },
+      },
+    }
   },
   args: {
     size: 'medium',
     disabled: false,
     readOnly: false,
+    switchedOnText: "",
+    switchedOffText: "",
+    width: undefined,
+    height: undefined,
   },
   render: (args) => ({
     props: args,
     template: `<dx-switch meSwitch ${argsToTemplate(args)}></dx-switch>`,
   }),
-} as Meta<MeLabelDirective | MeSwitchDirective | DxSwitchComponent>;
+} satisfies Meta<DxSwitchComponent | MeSwitchDirective | MeLabelDirective>;
 
-type Story = StoryObj<MeLabelDirective | MeSwitchDirective | DxSwitchComponent>;
+type Story = StoryObj<DxSwitchComponent | MeSwitchDirective | MeLabelDirective>;
 
 export const Default: Story = {};
 
+export const SizeSmall: Story = {
+  args: {
+    size: 'small'
+  }
+};
+
+export const SizeMedium: Story = {
+  args: {
+    size: 'medium'
+  }
+};
+
+export const SizeLarge: Story = {
+  args: {
+    size: 'large'
+  }
+};
+
+export const StateDisabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const StateReadOnly: Story = {
+  args: {
+    readOnly: true,
+  },
+};
+
+export const StateDisabledAndReadOnly: Story = {
+  args: {
+    readOnly: true,
+    disabled: true,
+  },
+};
+
 export const WithLabel: Story = {
-  args: {},
   render: (args) => ({
     props: args,
     template: `<label meLabel width="90px"><dx-switch meSwitch ${argsToTemplate(
       args
     )}></dx-switch>Switch</label>`,
   }),
+};
+
+export const WithSwitchStatusText: Story = {
+  args: {
+    switchedOnText: 'On',
+    switchedOffText: 'Off',
+  },
 };
