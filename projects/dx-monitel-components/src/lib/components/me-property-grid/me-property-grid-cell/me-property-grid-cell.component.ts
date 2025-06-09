f99a517baf5255e54be2b2adab6bcacc221fd9c8
Vitaly Astakhov <vitalastakhov@gmail.com>
2025-06-09 17:44:36 +0300

    feat(#866): add "panZoomOutMeX20" to property-grid-cell component

--- projects/dx-monitel-components/src/lib/components/me-property-grid/me-property-grid-cell/me-property-grid-cell.component.ts
+++ projects/dx-monitel-components/src/lib/components/me-property-grid/me-property-grid-cell/me-property-grid-cell.component.ts
@@ -12,7 +12,7 @@ import {
   inject,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { contentCopyX20, panZoomX20 } from '@monitel/me-icons';
+import { contentCopyX20, panZoomOutMeX20, panZoomX20 } from '@monitel/me-icons';
 import { MeIconsModule, MeIconsRegistry } from '@monitel/me-icons-registry';
 import { DxSplitterModule } from 'devextreme-angular';
 
@@ -77,10 +77,7 @@ export class MePropertyGridCellComponent implements AfterContentInit {
   hasAdditionalProperties = false;
 
   constructor() {
-    this.meIconRegistry.registerIcons([contentCopyX20, panZoomX20]);
-
-    // Закомментировано так как в библиотеке иконок сейчас нет иконки panZoomOut20
-    // meIconRegistry.registerIcons([panZoomOut20]);
+    this.meIconRegistry.registerIcons([contentCopyX20, panZoomX20, panZoomOutMeX20]);
   }
 
   ngAfterContentInit(): void {
