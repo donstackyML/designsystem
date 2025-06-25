9c6f860febe4fa4f70c04ec49e6c7e877a8a4c69
eugene <tommi.tomat@gmail.com>
2025-06-25 16:39:36 +0300

    fix(#279): breadcrumbs fixes

--- projects/dx-monitel-components/src/lib/components/me-breadcrumbs/me-breadcrumbs.component.ts
+++ projects/dx-monitel-components/src/lib/components/me-breadcrumbs/me-breadcrumbs.component.ts
@@ -310,6 +310,22 @@ export class MeBreadcrumbsComponent
 
       this.overflowMenu.instance.option({
         position: this.contextMenuPosition,
+        onShown: () => {
+          const popup = document.querySelector('.me-breadcrumbs-overflow-menu-popup') as HTMLElement;
+
+          if (popup && !popup.dataset['heightAdjusted']) {
+            const currentMaxHeight = popup.style.maxHeight;
+
+            if (currentMaxHeight) {
+              const currentValue = parseInt(currentMaxHeight);
+              if (!isNaN(currentValue)) {
+                console.log(currentValue);
+
+                popup.style.maxHeight = `${currentValue + 8}px`;
+              }
+            }
+          }
+        },
         ...this.overflowMenuOptions,
       });
 
@@ -326,6 +342,7 @@ export class MeBreadcrumbsComponent
     }
 
     const viewportHeight = document.documentElement.clientHeight;
+
     if (menuRect.bottom > viewportHeight) {
       e.position.my = e.position.my.replace('top', 'bottom');
       e.position.at = e.position.at.replace('bottom', 'top');
@@ -430,7 +447,7 @@ export class MeBreadcrumbsComponent
     const buttonsWidth = this.getOverflowButtonsWidth();
     const availableWidth = containerWidth - buttonsWidth;
 
-    const { visibleCount, accumulatedWidth } = visibleItems.reduce(
+    const { visibleCount } = visibleItems.reduce(
       (result, item) => {
         if (result.shouldContinue) {
           const itemWidth = item.nativeElement.offsetWidth;
