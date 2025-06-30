51e4d9c1adb5bdb6c47078e8c37bec521e793c36
MaxMiroslavskiy <maksimsaeros@gmail.com>
2025-06-30 17:05:08 +0500

    fix(#826): warning button type

--- projects/dx-monitel-components/src/documentation/storybook/stories/me-button/me-button.stories.ts
+++ projects/dx-monitel-components/src/documentation/storybook/stories/me-button/me-button.stories.ts
@@ -151,7 +151,7 @@ export default {
     },
   },
   args: {
-    type: 'normal',
+    buttonType: 'normal',
     text: 'Button',
     size: 'medium',
     stylingMode: 'contained',
@@ -191,31 +191,31 @@ export const SizeLarge: Story = {
 
 export const TypeNormal: Story = {
   args: {
-    type: 'normal',
+    buttonType: 'normal',
   },
 };
 
 export const TypeDefault: Story = {
   args: {
-    type: 'default',
+    buttonType: 'default',
   },
 };
 
 export const TypeSuccess: Story = {
   args: {
-    type: 'success',
+    buttonType: 'success',
   },
 };
 
 export const TypeWarning: Story = {
   args: {
-    type: 'warning',
+    buttonType: 'warning',
   },
 };
 
 export const TypeDanger: Story = {
   args: {
-    type: 'danger',
+    buttonType: 'danger',
   },
 };
 
