0bba4fde2b0154f540de56cb61ef3236b2cb06c6
eugene <tommi.tomat@gmail.com>
2025-07-01 14:22:30 +0300

    fix(#908): me menu fixes

--- projects/dx-monitel-components/src/documentation/storybook/stories/me-drop-down-box/me-drop-down-box.stories.ts
+++ projects/dx-monitel-components/src/documentation/storybook/stories/me-drop-down-box/me-drop-down-box.stories.ts
@@ -253,6 +253,7 @@ export const WithDataGrid: Story = {
           meDataGrid
           [dataSource]="dataSource"
           [columns]="['id', 'name']"
+          [selection]="{ mode: 'multiple' }"
           [hoverStateEnabled]="true"
           [filterRow]="{ visible: true }"
           [height]="300"
