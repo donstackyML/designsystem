8316687a40cd29120dd8a55b69d3cef9feb3e951
MaxMiroslavskiy <maksimsaeros@gmail.com>
2025-07-18 13:32:58 +0500

    feat(#982): table scroll

--- projects/dx-monitel-components/src/documentation/storybook/stories/me-tree-list/me-tree-list.stories.ts
+++ projects/dx-monitel-components/src/documentation/storybook/stories/me-tree-list/me-tree-list.stories.ts
@@ -200,13 +200,13 @@ export default {
 
 		<dxo-selection [mode]="mode"></dxo-selection>
 
-    <dxi-column dataField="Full_Name">
+    <dxi-column dataField="Full_Name" [width]="120">
       <dxi-validation-rule type="required"></dxi-validation-rule>
     </dxi-column>
-    <dxi-column dataField="Head_ID" caption="Head">
+    <dxi-column dataField="Head_ID" caption="Head" [width]="120">
       <dxi-validation-rule type="required" message="Head is very very very very very very very very very very very required"></dxi-validation-rule>
     </dxi-column>
-    <dxi-column dataField="Title" caption="Position">
+    <dxi-column dataField="Title" caption="Position" [width]="120">
       <dxi-validation-rule type="required"></dxi-validation-rule>
     </dxi-column>
     <dxi-column dataField="Hire_Date" dataType="date" [width]="120">
@@ -220,6 +220,7 @@ export default {
 
       <dxi-button name="undelete" icon="undelete"></dxi-button>
     </dxi-column>
+    <dxi-column [width]="800" [allowResizing]="false" cellTemplate="empty"></dxi-column>
   </dx-tree-list>
 		`,
   }),
