cb307c7b077bc97c84f2e96a4e49eeb07d1f11cb
eugene <tommi.tomat@gmail.com>
2025-08-12 17:26:01 +0300

    fix(#1118): dropdownbox ts error fix

--- projects/dx-monitel-components/src/lib/directives/me-drop-down-box/me-drop-down-box.directive.ts
+++ projects/dx-monitel-components/src/lib/directives/me-drop-down-box/me-drop-down-box.directive.ts
@@ -144,17 +144,6 @@ export class MeDropDownBoxDirective
     );
   }
 
-  private arraysEqual(a?: any[], b?: any[]) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length !== b.length) return false;
-
-    for (let i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  }
-
   private setLeftIcon() {
     if (this.leftIcon) {
       const textEditorContainer = this.element.nativeElement.querySelector(
