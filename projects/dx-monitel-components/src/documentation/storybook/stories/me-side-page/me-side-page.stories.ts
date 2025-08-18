e304eb2d04d566a3c97ab19e4a472342700bc47b
MaxMiroslavskiy <maksimsaeros@gmail.com>
2025-08-18 19:17:58 +0500

    Merge branch 'add-story-v24' of ssh://tfs-ext.sandbox.local:22/CK21_External/DesignSystem/_git/DesignSystem into fix/863-side-page-fixes

--- projects/dx-monitel-components/src/documentation/storybook/stories/me-side-page/me-side-page.stories.ts
+++ projects/dx-monitel-components/src/documentation/storybook/stories/me-side-page/me-side-page.stories.ts
@@ -60,7 +60,7 @@ import {
       <div sidepage-content class="me-sidepage-content">
         <!-- Checkboxes Section -->
         <div class="settings-section">
-          <h3 class="me-title-header2">Основны е параметры</h3>
+          <h3 class="me-title-header2">Основные параметры</h3>
           <div class="checkbox-group">
             <label meLabel labelDirection="row" class="checkbox-item">
               <dx-check-box
